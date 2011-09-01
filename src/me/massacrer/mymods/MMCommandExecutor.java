2ecdc4d378d82f0ddd6706f7e375e8b23f4e4ef2
Massacrer <massacrer@hotmail.co.uk>
2011-09-01 20:51:49 +0100

    Implemented redstone remote activation

--- src/me/massacrer/mymods/MMCommandExecutor.java
+++ src/me/massacrer/mymods/MMCommandExecutor.java
@@ -16,21 +16,56 @@ class MMCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-		if(!(sender instanceof Player)) {
+		if (!(sender instanceof Player)) {
 			dealWithConsole(sender);
 		} else {
 			Player player = (Player) sender;
+			
 			if (args.length > 0) {
-				if (args[0].equalsIgnoreCase("skin") || args[0].equalsIgnoreCase("a")) {
-					plugin.fitCustomArmour(player, args);
+				if (args[0].equalsIgnoreCase("hat")
+						|| args[0].equalsIgnoreCase("a")) {
+					if (args.length > 3) {
+						Player p = plugin.getServer().getPlayer(args[1]);
+						if (p != null) {
+							plugin.fitCustomHat(p, args[1]);
+						} else {
+							player.sendMessage(ChatColor.RED
+									+ "Invalid player name or syntax. Syntax is <command> hat <player> <block>");
+						}
+						return true;
+					}
+					plugin.fitCustomHat(player, args[1]);
+					return true;
+				}
+				if (args[0].equalsIgnoreCase("r")) {
+					Boolean snipe;
+					Player activePlayer;
+					
+					if (args.length > 1) {
+						snipe = false;
+						activePlayer = plugin.getServer().getPlayer(args[1]);
+						if (activePlayer == null) {
+							sender.sendMessage(ChatColor.RED
+									+ "Player not found");
+							return true;
+						}
+					} else {
+						snipe = true;
+						activePlayer = player;
+					}
+					plugin.redstoneSnipe(activePlayer, snipe);
+					return true;
 				}
 			} else {
 				player.sendMessage(ChatColor.DARK_AQUA + "MPack is active");
 			}
 		}
+		
 		return false;
 	}
+	
 	private void dealWithConsole(CommandSender sender) {
-		//TODO: deal with console
+
+		// TODO: deal with console
 	}
 }
--- src/me/massacrer/mymods/MassacrersMod.java
+++ src/me/massacrer/mymods/MassacrersMod.java
@@ -1,134 +1,113 @@
 package me.massacrer.mymods;
 
-import java.util.HashMap;
-import java.util.logging.Logger;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class MassacrersMod extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
 	MMCommandExecutor commandExecutor = new MMCommandExecutor(this);
-	PluginManager pm = getServer().getPluginManager();
+	PluginManager pm;
 	
 	public void onDisable() {
 		log.info("Massacrer's Mod disabled");
 	}
 	
 	public void onEnable() {
-		getCommand("m").setExecutor(commandExecutor);
+		pm = getServer().getPluginManager();
 		getCommand("mm").setExecutor(commandExecutor);
-		getCommand("mmod").setExecutor(commandExecutor);
 		log.info("Massacrer's Mod enabled");
 	}
 	
-	void fitCustomArmour(Player player, String[] args) {
-		// args[0] is the armour command string
-		if (args.length < 3) {
+	void fitCustomHat(Player player, String str_block) {
+		int materialId = 0;
+		try {
+			materialId = Integer.parseInt(str_block);
+		} catch (NumberFormatException e) {
 			reportInvalidInput(player);
 			return;
-		} else {
-			int armourSlot = getPlayerSlot(args[1]);
-			int materialId = -1;
+		}
+		
+		ItemStack is = new ItemStack(materialId, 1);
+		PlayerInventory inv = player.getInventory();
+		
+		if (inv.getHelmet().getTypeId() >= 256) {
+			HashMap<Integer, ItemStack> items = inv.addItem(inv.getHelmet());
 			
-			if (armourSlot == 0) {
-				reportInvalidInput(player);
-				return;
-			}
-			
-			try {
-				materialId = Integer.parseInt(args[2]);
-			} catch (NumberFormatException ex) {}
-			if (materialId == -1) {
-				materialId = Material.matchMaterial(args[2]).getId();
-			}
-			if (materialId < 1) {
-				player.sendMessage(ChatColor.RED
-						+ "Material must be valid in either string or integer form");
-				return;
-			}
-			if (materialId >= 256) {
-				player.sendMessage(ChatColor.RED
-						+ "Material value is invalid; specify one between 0 and 256 for valid results");
-				return;
-			}
-			
-			ItemStack is = new ItemStack(materialId, 1);
-			PlayerInventory inv = player.getInventory();
-			
-			if (armourSlot == 1) {
-				if (inv.getHelmet().getTypeId() >= 256) {
-					HashMap<Integer, ItemStack> items = inv.addItem(inv
-							.getHelmet());
-					for (int i = 0; i < items.size(); i++) {
-						player.getWorld().dropItemNaturally(
-								player.getEyeLocation(), items.get(i));
-					}
-				}
-				inv.setHelmet(is);
-			}
-			if (armourSlot == 2) {
-				if (inv.getChestplate().getTypeId() >= 256) {
-					HashMap<Integer, ItemStack> items = inv.addItem(inv
-							.getChestplate());
-					for (int i = 0; i < items.size(); i++) {
-						player.getWorld().dropItemNaturally(
-								player.getEyeLocation(), items.get(i));
-					}
-				}
-				inv.setChestplate(is);
-			}
-			if (armourSlot == 3) {
-				if (inv.getLeggings().getTypeId() >= 256) {
-					HashMap<Integer, ItemStack> items = inv.addItem(inv
-							.getLeggings());
-					for (int i = 0; i < items.size(); i++) {
-						player.getWorld().dropItemNaturally(
-								player.getEyeLocation(), items.get(i));
-					}
-				}
-				inv.setLeggings(is);
-			}
-			if (armourSlot == 4) {
-				if (inv.getBoots().getTypeId() >= 256) {
-					HashMap<Integer, ItemStack> items = inv.addItem(inv
-							.getBoots());
-					for (int i = 0; i < items.size(); i++) {
-						player.getWorld().dropItemNaturally(
-								player.getEyeLocation(), items.get(i));
-					}
-				}
-				inv.setBoots(is);
+			for (int i = 0; i < items.size(); i++) {
+				player.getWorld().dropItemNaturally(player.getEyeLocation(),
+						items.get(i));
 			}
 		}
+		
+		inv.setHelmet(is);
+		
 	}
 	
 	boolean isAllowed(Player player) {
-		if (player.hasPermission("massacrer.armour"))
+		if (player.hasPermission("massacrer.armour")) {
 			return true;
-		if (player.isOp())
+		}
+		
+		if (player.isOp()) {
 			return true;
+		}
+		
 		return false;
 	}
 	
 	int getPlayerSlot(String str) {
-		if (str.equalsIgnoreCase("head"))
+		if (str.equalsIgnoreCase("head")) {
 			return 1;
-		if (str.equalsIgnoreCase("body") || str.equalsIgnoreCase("chest"))
+		}
+		
+		if (str.equalsIgnoreCase("body") || str.equalsIgnoreCase("chest")) {
 			return 2;
-		if (str.equalsIgnoreCase("legs"))
+		}
+		
+		if (str.equalsIgnoreCase("legs")) {
 			return 3;
-		if (str.equalsIgnoreCase("feet") || str.equalsIgnoreCase("boots"))
+		}
+		
+		if (str.equalsIgnoreCase("feet") || str.equalsIgnoreCase("boots")) {
 			return 4;
+		}
+		
 		return 0;
 	}
 	
 	void reportInvalidInput(Player p) {
 		p.sendMessage(ChatColor.DARK_AQUA
 				+ "Specify correct arguments for this function");
+	}
+	
+	void redstoneSnipe(Player player, Boolean snipe) {
+		Block target;
+		if (snipe) {
+			target = player.getTargetBlock(null, 200);
+		} else {
+			Location loc = player.getLocation();
+			loc.setY(loc.getY() - 1);
+			target = loc.getBlock();
+		}
+		boolean valid = false;
+		if ((target.getType() == Material.REDSTONE)
+				|| (target.getType() == Material.REDSTONE_WIRE)
+				|| (target.getType() == Material.REDSTONE_TORCH_OFF)
+				|| (target.getType() == Material.TNT)) {
+			valid = true;
+		}
+		if(valid) {
+			target.setData((byte) 15);
+		}
 	}
 }
